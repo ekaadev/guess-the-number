ca23c40073d877a4c0ea25b030a4bac211025f32
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-01-01 14:36:29 +0700

    fix: condition outside the rules

--- src/Main.java
+++ src/Main.java
@@ -8,45 +8,52 @@ public class Main {
         int chance = 3;
         int randomNum = random();
 
-
+        System.out.println("======================================================");
+        System.out.println("Guess The Number");
         System.out.println("Menebak angka dari 1 - 10");
+        System.out.println("======================================================");
 
         while (chance >= 1) {
             int inputNum = input(sc);
 
-            if (inputNum < randomNum) {
-                System.out.println("Terlalu rendah");
+            if (inputNum > 10 || inputNum == 0) {
+                System.out.println("alert: Anda kalah");
+                System.out.println("alert: Angka yang berikan melewati batas!");
+                chance = 0;
+
+            } else if (inputNum < randomNum) {
+                System.out.println("alert: Angka yang anda berikan terlalu rendah!");
                 chance--;
 
 
                 if ( chance == 0 ) {
-                    System.out.println("Anda kalah");
+                    System.out.println("alert: Anda kalah");
                 } else {
-                    System.out.println("Kesempatannmu tersisa " + chance);
+                    System.out.println("alert: Kesempatannmu tersisa " + chance);
+                    System.out.println("======================================================");
                 }
 
 
-            } else if (inputNum > 10) {
-                System.out.println("Angka yang berikan melewati batas");
-                chance = 0;
-
             } else if (inputNum > randomNum) {
-                System.out.println("Terlalu tinggi");
+                System.out.println("alert: Angka yang anda berikan terlalu tinggi!");
                 chance--;
 
 
                 if ( chance == 0 ) {
-                    System.out.println("Anda kalah");
+                    System.out.println("alert: Anda kalah!");
                 } else {
-                    System.out.println("Kesempatannmu tersisa " + chance);
+                    System.out.println("alert: Kesempatannmu tersisa " + chance);
+                    System.out.println("======================================================");
                 }
 
             } else {
-                System.out.println("Anda benar");
+                System.out.println("Selamat anda menang! Angkanya adalah " + randomNum);
                 chance = 0;
             }
 
         }
+
+        sc.close();
     }
 
     public static int random() {
